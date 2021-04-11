adfdeb1df3a86497c792446382da8ce28e2b256c
Jessica Leverett <Jessak1202@gmail.com>
2021-04-11 17:09:11 -0500

    OrdeR HISTORY

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    lightBg: false,
+    primary: true,
+    lightBg: false,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialists can help you achieve your business goals and grow your brand. ',
+    headline: 'Lead generation Specialists for online businesses',
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: require('../../images/svg-1.svg'),
+    alt: 'Image',
+    start: ''
+}
